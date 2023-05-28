26ea066d63cff328a534c415385f952d6789d07e
EderV <ederv1009@gmail.com>
2023-05-28 13:17:41 +0200

    Added profiles to pom.xml

--- src/main/java/com/fondo/bufero/WebCalendar/WebCalendarApplication.java
+++ src/main/java/com/fondo/bufero/WebCalendar/WebCalendarApplication.java
@@ -2,9 +2,16 @@ package com.fondo.bufero.WebCalendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class WebCalendarApplication {
+public class WebCalendarApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(WebCalendarApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebCalendarApplication.class, args);
