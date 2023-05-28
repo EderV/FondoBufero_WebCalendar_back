95967519668ec0678f5109c52e768269b5d198ff
EderV <ederv1009@gmail.com>
2023-05-28 15:46:03 +0200

    Added event table in database Added database_tables for storing the create table querys

--- src/main/java/com/fondo/bufero/WebCalendar/application/TestController.java
+++ src/main/java/com/fondo/bufero/WebCalendar/application/TestController.java
@@ -1,16 +1,39 @@
 package com.fondo.bufero.WebCalendar.application;
 
+import com.fondo.bufero.WebCalendar.domain.EventEntity;
+import com.fondo.bufero.WebCalendar.infrastructure.repository.EventRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Calendar;
+import java.util.Date;
+
 @RestController
 @RequestMapping("/test")
+@RequiredArgsConstructor
 public class TestController {
+
+    private final EventRepository eventRepository;
 
     @GetMapping
     public ResponseEntity<String> helloWorld() {
+
+        eventRepository.save(EventEntity.builder()
+                        .title("Evento moonsama mansion")
+                        .description("asdgasdrfhgadfhadf")
+                        .owner("Greensama")
+                        .logo("/resources/logos/greensama.png")
+                        .date(new Date(2023, Calendar.SEPTEMBER, 15, 16, 30))
+                        .duration(120)
+                        .type("Poker")
+                        .link("https://www.greemsama.com")
+                        .canceled(true)
+                        .cancelReason("Testing event cancellation")
+                        .build());
+
         return ResponseEntity.ok("Hello world");
     }
 
--- src/main/java/com/fondo/bufero/WebCalendar/domain/EventEntity.java
+++ src/main/java/com/fondo/bufero/WebCalendar/domain/EventEntity.java
@@ -5,8 +5,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.GenericGenerator;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Data
@@ -18,12 +19,40 @@ import java.util.UUID;
 public class EventEntity {
 
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    @Column(name = "id",
-            updatable = false,
-            nullable = false,
-            columnDefinition = "VARCHAR(36)")
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    private String title;
+
+    private String description;
+
+    private String owner;
+
+    private String logo;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "dd/MM/yyyy hh:mm:ss")
+    private Date date;
+
+    private Integer duration;   // In minutes
+
+    private String type;
+
+    private String link;
+
+    private Boolean canceled;
+
+    @Column(name = "cancel_reason")
+    private String cancelReason;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @DateTimeFormat(pattern = "dd/MM/yyyy hh:mm:ss")
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = new Date();
+    }
 
 }
